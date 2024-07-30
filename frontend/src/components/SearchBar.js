4f2b21c1f12c7c67d59b2028fcd1d1c87bce4035
alexis <alenegro0512@gmail.com>
2024-07-30 18:27:39 -0300

    agregado de search bar

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div className="flex justify-end w-full mb-4">
+      <div className="w-1/6">
+        <TextField
+          variant="outlined"
+          value={query}
+          onChange={handleInputChange}
+          placeholder="Buscar productos..."
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleSearch}>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          fullWidth
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
