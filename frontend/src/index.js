452f840a0b06f2b13ed85c647c2c6d025237db5f
alexis <alenegro0512@gmail.com>
2024-08-03 01:00:14 -0300

    agregando home

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Products from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-      <Products />
-      
-
+      <App />
     
   </React.StrictMode>
 );
---
+++ frontend/src/views/Home.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../index.css';
+import ProductCard from '../components/Card'; // Asegúrate de que la ruta sea correcta
+import SearchBar from '../components/SearchBar'; // Importa el componente SearchBar
+
+function Products() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    // Reemplaza 'http://localhost:8000/products' con la URL de tu API
+    axios.get('http://127.0.0.1:8000/products')
+      .then(response => {
+        setProducts(response.data.products);
+        setFilteredProducts(response.data.products);
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+  const handleSearch = (query) => {
+    const filtered = products.filter(product =>
+      product.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
+  return (
+    <div className="background flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-6xl font-bold text-black-500 mb-4">Morphic Threads</h1>
+      <SearchBar onSearch={handleSearch} />
+      {filteredProducts.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {filteredProducts.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <p>Cargando productos...</p>
+      )}
+    </div>
+  );
+}
+
+export default Products;
