fbe67d2d1ba2527a7d1cbfedd078080a91c5c492
alexis120595 <alenegro0512@gmail.com>
2024-12-28 00:09:46 -0300

    actualizacion 3

--- main.py
+++ main.py
@@ -78,5 +78,5 @@ app.add_middleware(
 
 if __name__ == '__main__':
   
-   port = int(os.getenv("PORT", 8000))
-uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
