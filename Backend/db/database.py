9f81424b7c6bcf7515848ab7e4b5bf07dc036583
alexis120595 <alenegro0512@gmail.com>
2024-12-28 00:15:07 -0300

    actualizacion 5

--- Backend/db/database.py
+++ Backend/db/database.py
@@ -7,17 +7,14 @@ from dotenv import load_dotenv
 # Cargar variables de entorno desde .env
 load_dotenv()
 
-# Obtener variables de entorno
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+# Obtener la URL de conexión desde la variable de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Construir la URL de conexión
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if not DATABASE_URL:
+    raise ValueError("La variable de entorno DATABASE_URL no está definida.")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Crear el engine de SQLAlchemy
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
